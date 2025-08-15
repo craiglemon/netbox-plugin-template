ded61149130c82f4b743c4ed42f856a4c6ef8e2d
Craig Lemon <Craig.Lemon@gmail.com>
2025-08-15 21:27:38 +1200

    Further enhancements - getting closer :-)

--- .devcontainer/dev-entrypoint.sh
+++ .devcontainer/dev-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+exec > /var/log/dev-entrypoint.log 2>&1
+
 # 1. Create symbolic link from our plugins source to the netbox plugins directory. 
 #   This allows us to develop plugins in the workspace and have them available in NetBox 
 #       without needing to rebuild the container.
@@ -21,8 +23,23 @@ echo "Running database migrations..."
 
 # 4. Collect static files
 echo "Collecting static assets..."
-/opt/netbox/venv/bin/python3 /opt/netbox/netbox/manage.py collectstatic --noinput
+/opt/netbox/venv/bin/python3 /opt/netbox/netbox/manage.py collectstatic --no-input
 
-# 5. Start the NetBox service
+# 5. Create superuser if not exists
+/opt/netbox/venv/bin/python3 << END
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get("SUPERUSER_NAME", "admin")
+email = os.environ.get("SUPERUSER_EMAIL", "admin@example.com")
+password = os.environ.get("SUPERUSER_PASSWORD", "admin")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+
+END
+
+# 6. Start the NetBox service
 echo "Starting NetBox development server..."
 exec "$@"
