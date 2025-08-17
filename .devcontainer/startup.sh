62b585f31c642cfc5b9a61a320a93f38ca8b9fec
Craig Lemon <Craig.Lemon@gmail.com>
2025-08-17 17:20:37 +1200

    Moved registration of plugins to register-plugins.sh that gets executed in postCreateCommand

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 chmod +x /workspace/netbox/dev-entrypoint.sh
+chmod +x /workspace/netbox/register-plugins.sh
+
 exec /workspace/netbox/dev-entrypoint.sh "$@"
---
+++ netbox/register-plugins.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+. /opt/netbox/venv/bin/activate
+
+pip install --no-cache-dir -e /opt/netbox/netbox/plugins/netbox_demo_plugin
